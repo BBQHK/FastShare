f9e87f80d42c1c1baabd0ea35511f3199afbc966
長崎岸久 <ngoscar9008@gmail.com>
2023-06-09 23:38:59 +0800

    init project

---
+++ front-end/FastShare/src/layouts/MainLayout.vue
@@ -0,0 +1,124 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header style="color:#000000; background-color: #ffffff;" elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          <q-img style="width: 150px;" src="..\assets\logo.png" />
+        </q-toolbar-title>
+
+        <div class="text-weight-bold">FastShare {{ app_version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated>
+        <q-toolbar style="color:#000000; background-color: #ffffff;">
+          <q-toolbar-title>Share any file to anyone.</q-toolbar-title>
+        </q-toolbar>
+    </q-footer>
+    
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false);
+    const app_version = 'v1.0.0';
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      app_version,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ front-end/FastShare/src/pages/IndexPage.vue
@@ -0,0 +1,73 @@
+<template>
+  <q-page class="main">
+    <div class="row">
+      <div class=".col">
+        <q-card class="share-card">
+          <q-card-section class="text-black">
+            <div class="text-h6 text-weight-bolder">Share</div>
+          </q-card-section>
+
+          <q-card-section>
+            <q-file color="deep-purple-12" filled v-model="model" label="Drop or select files here" multiple>
+              <template v-if="model" v-slot:append>
+                <q-icon name="cancel" @click.stop.prevent="model = null" class="cursor-pointer"></q-icon>
+              </template>
+            </q-file>
+          </q-card-section>
+
+          <q-separator />
+
+          <q-card-actions align="right">
+            <q-btn flat>Upload</q-btn>
+          </q-card-actions>
+        </q-card>
+
+        <q-card class="receive-card">
+          <q-card-section class="text-black">
+            <div class="text-h6 text-weight-bolder">Receive</div>
+          </q-card-section>
+          <q-card-section>
+            <q-input filled v-model="ReceiveCode" label="Enter code here" />
+          </q-card-section>
+          <q-separator />
+
+          <q-card-actions align="right">
+            <q-btn flat>Downlaod</q-btn>
+          </q-card-actions>
+        </q-card>
+      </div>
+      <div class=".col">
+        <!--insert logo-->
+        <q-img style="width: 500px;" src="..\assets\logo.png" />
+        <div class="text-h5" style="margin-left: 40px; color: white;">
+          Share and download files easily with FastShare, your go-to file sharing platform.
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<style scoped>
+.share-card, .receive-card {
+  margin: 10px;
+  height: 200px;
+  width: 350px;
+  margin-left: 100px;
+}
+
+.share-card{
+  margin-top: 100px;
+}
+
+.main {
+  background-color: rgb(102, 20, 255);
+}
+</style>
+
+<script setup>
+import { ref } from 'vue'
+
+const model = ref(null);
+const ReceiveCode = ref('');
+
+</script>
