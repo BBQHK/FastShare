7b4b41d292be52cdc5269eb5d3dd1eb3e1fced25
oscar.ng <oscar.ng@hactl.com>
2023-11-02 14:19:42 +0800

    create signalling consumer for webRTC

--- back-end/FastShare/fileupload/routing.py
+++ back-end/FastShare/fileupload/routing.py
@@ -5,4 +5,5 @@ from . import consumers
 websocket_urlpatterns = [
     # re_path(r'(?P<receive_Code>\w+)/$', consumers.FileDownloadConsumer.as_asgi()),
     re_path(r'^ws/download_status/(?P<receive_Code>\w+)/$', consumers.FileDownloadConsumer.as_asgi()),
+    re_path(r'^ws/p2p_signal/(?P<receive_Code>\w+)/$', consumers.SignallingConsumer.as_asgi()),
 ]
